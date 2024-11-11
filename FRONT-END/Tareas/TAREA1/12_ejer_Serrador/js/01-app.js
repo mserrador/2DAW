e69b1dbd7a87cbe2757bc7a45944714fcbee80d9
Mario <5902919@alu.murciaeduca.es>
2024-11-11 16:19:42 +0100

    actualizacion tarea1 js

--- FRONT-END/Tareas/TAREA1/12_ejer_Serrador/js/01-app.js
+++ FRONT-END/Tareas/TAREA1/12_ejer_Serrador/js/01-app.js
@@ -1,10 +1,18 @@
 "use strict"
-
-console.log("ibai ya no ahce gracia porque esta delgado")
-
-
+// Añade al prototipo del objeto Array una función llamada cuantasVecesEsta que devuelva el
+// número de veces que está un número dado en un array. Pruébala.
 
 
+Array.prototype.cuantasVecesEsta = function (numero) {
+    let contador = 0;
+    for (let i = 0; i < this.length; i++) {
+        if (this[i] === numero) {
+            contador++;
+        };
+    };
+    console.log(`el numero ${numero} aparace ${contador} veces`);
+};
 
+const pruebaArray = [8,4,6,9,1,1,1,1,1,2,4,4,4,4,7];
 
-
+pruebaArray.cuantasVecesEsta(1);
--- FRONT-END/Tareas/TAREA1/13_ejer_Serrador/js/01-app.js
+++ FRONT-END/Tareas/TAREA1/13_ejer_Serrador/js/01-app.js
@@ -1,1 +1,20 @@
 "use strict"
+//  Añade al prototipo del objeto Date una función llamada añoNuevo que devuelva el número
+// de días que queda hasta el 1 de Enero del siguiente año desde el día que reciba como
+// parámetro (Nota. Trabajad con milisegundos). Pruébala
+
+
+Date.prototype.añoNuevo = function() {
+
+    const proximoAñoNuevo = new Date(this.getFullYear() + 1, 0, 1);
+    
+    const diferencia = proximoAñoNuevo - this;
+    
+    const diasRestantes = Math.ceil(diferencia / (1000 * 60 * 60 * 24));
+    
+    return diasRestantes;
+};
+
+
+const fecha = new Date(); 
+console.log(`Días restantes hasta el 1 de Enero del próximo año: ${fecha.añoNuevo()}`);
